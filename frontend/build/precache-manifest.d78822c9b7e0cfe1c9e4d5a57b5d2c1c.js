1709e104c47824c744326d211a6800376e5cf2fd
cocotums <pastitumbuck@gmail.com>
2020-09-03 15:18:06 +0800

    Add built frontend app

---
+++ frontend/build/precache-manifest.d78822c9b7e0cfe1c9e4d5a57b5d2c1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8226eb668ae1ebeeb5adf104c83b59e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fa8171bc15ade2c73f4",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "99eda3e2eed958bfa9d9",
+    "url": "/static/css/main.d0fbc0e9.chunk.css"
+  },
+  {
+    "revision": "3fa8171bc15ade2c73f4",
+    "url": "/static/js/2.b9eabcb9.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.b9eabcb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99eda3e2eed958bfa9d9",
+    "url": "/static/js/main.31ac9f05.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
